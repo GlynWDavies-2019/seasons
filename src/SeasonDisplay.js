008047e39b048942dacc1ddca05a6af0ec5745d5
Glyn Davies <glynwdavies@gmail.com>
2020-09-07 08:08:43 +0100

    Customised SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,21 +1,37 @@
 import React from 'react';
 
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brrr, its chilly outside',
+        iconName: 'snowflake'
+    }
+}
+
 const getSeason = (latitude,month) => {
     const hemisphere = latitude > 0 ? 'N' : 'S';
     if(month > 2 && month < 9) {
-        return hemisphere === 'N' ? 'Summer' : 'Winter';
+        return hemisphere === 'N' ? 'summer' : 'winter';
     } else {
-        return hemisphere === 'S' ? 'Winter' : 'Summer';
+        return hemisphere === 'S' ? 'winter' : 'summer';
     }
 };
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.latitude,new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
     return (
-        <div>
-            Season: {season}
-        </div>
-    );
+            <div className={`season-display ${season}`}>
+                <i className={`icon-left massive ${iconName} icon`} />
+                <h1>{text}</h1>
+                <i className={`icon-right massive ${iconName} icon`} />
+            </div>
+    )
 };
 
 export default SeasonDisplay;
